aaab032b3f608c6030879e964e22ccfc053e52c6
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-10-21 10:43:20 -0400

    fetch GrapQL from React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import fetchGraphQL from './fetchGraphQL';
+
+const { useState, useEffect } = React;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // We'll load the name of a repository, initially setting it to null
+    const [name, setName] = useState(null);
+
+    // When the component mounts we'll fetch a repository name
+    useEffect(() => {
+        let isMounted = true;
+        fetchGraphQL(`
+        query RepositoryNameQuery {
+        repository(owner: "facebook", name: "relay") {
+            name
+        }
+        }
+    `).then(response => {
+            // Avoid updating state if the component unmounted before the fetch completes
+            if (!isMounted) {
+                return;
+            }
+            const data = response.data;
+            setName(data.repository.name);
+        }).catch(error => {
+            console.error(error);
+        });
+
+        return () => {
+            isMounted = false;
+        };
+    }, [fetchGraphQL]);
+
+    // Render "Loading" until the query completes
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    {name != null ? `Repository: ${name}` : "Loading"}
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
